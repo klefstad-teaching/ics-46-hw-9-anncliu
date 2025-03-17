e95bd116c348a4ae385fe1eb7172b82593c074dd
anncliu <anncl1@uci.edu>
2025-03-17 00:40:13 +0000

    dijkstras_shortest_path() passes test for small graph

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -4,9 +4,44 @@
 #include "ladder.h"
 
 
+vector<int> run_dijkstras(string file_name, int source)
+{
+  Graph g;
+  vector<int> prev;
+  file_to_graph(file_name, g);
+  return dijkstra_shortest_path(g, source, prev);
+}
+
+void are_equal(int expected[], int arr_size, const vector<int>& distances)
+{
+  EXPECT_EQ(arr_size, distances.size());
+
+  for (int i = 0; i < arr_size; ++i)
+    EXPECT_EQ(expected[i], distances[i]);
+}
+
 
 TEST(DijkstrasAlgorithmTest, print_path) {
   int arr[] = {4, 13, 9, 6, 12, 2};
   vector<int> v(arr, arr+sizeof(arr)/sizeof(int));
   print_path(v, 10); // prints "4 13 9 6 12 2 \nTotal cost is 10\n"
+}
+
+
+TEST(DijkstrasAlgorithmTest, ShortestPathSmallGraph)
+{
+  int expected[] = {0, 3, 6, 1};
+  are_equal(expected, 4, run_dijkstras("src/small.txt", 0));
+}
+
+TEST(DijkstrasAlgorithmTest, ShortestPathMediumGraph)
+{
+  int expected[] = {0, 5, 3, 12, 10, 2};
+  are_equal(expected, 6, run_dijkstras("src/medium.txt", 0));
+}
+
+TEST(DijkstrasAlgorithmTest, ShortestPathLargeGraph)
+{
+  int expected[] = {0, 7, 15, 5, 14, 11, 22};
+  are_equal(expected, 7, run_dijkstras("src/large.txt", 0));
 }
--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,4 +1,72 @@
+#include <utility>
+
 #include "dijkstras.h"
+
+
+using Vertex = int;
+using Distance = int;
+
+struct DistanceComparer
+{
+    bool operator () (const pair<Vertex, Distance>& lhs, const pair<Vertex, Distance>& rhs) const
+    {
+        return get<1>(lhs) < get<1>(rhs);
+    }
+};
+
+using distanceMinHeap = priority_queue< pair<Vertex, Distance>, vector< pair<Vertex, Distance>>, DistanceComparer >;
+
+
+void check_neighbors_weight(const Edge e, vector<Distance>& distances, vector<int>& previous, 
+                            vector<bool>& visited, distanceMinHeap& pq)
+{
+    Vertex curr = e.src;
+    Vertex neighbor = e.dst;
+    Distance new_distance = distances[curr] + e.weight;
+
+    if (!visited[neighbor] && new_distance < distances[neighbor])
+    {
+        distances[neighbor] = new_distance;
+        previous[neighbor] = curr;
+        pq.push({neighbor, new_distance});
+    }
+}
+
+
+void visit_next(const Graph& G, vector<Distance>& distances, vector<int>& previous, 
+                vector<bool>& visited, distanceMinHeap& pq)
+{
+    while (!pq.empty())
+    {
+        Vertex v = get<0>(pq.top());
+        pq.pop();
+
+        if (!visited[v])
+        {
+            visited[v] = true;
+            for (Edge e : G[v])
+                check_neighbors_weight(e, distances, previous, visited, pq);
+        }
+    }
+}
+
+
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous)
+{
+    int n = G.numVertices;
+
+    vector<Distance> distances(n, INF);
+    previous.assign(n, -1);
+    vector<bool> visited(n, false);
+
+    distanceMinHeap pq;
+    pq.push({source, 0});
+    distances[source] = 0;
+
+    visit_next(G, distances, previous, visited, pq);
+
+    return distances;
+}
 
 
 void print_path(const vector<int>& v, int total)
